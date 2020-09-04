89adee8af70bfd5f7a8479f4bffd72d685fbec47
Daniel lemes <inventarartcomunicacao@hotmail.com>
2020-09-04 16:20:05 -0300

    paginaHome-Daniel-Jessica

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,11 +1,47 @@
-import React, { Component } from 'react'
+import React from "react";
+import Home from "./Home";
+import NovaPagina from "./NovaPagina";
+// import MenuNavBar from "./components/NavBar";
+// import Footer from "./components/Footer";
+import Services from "./Services";
 
-export class AppContainer extends Component {
+class App extends React.Component {
+  state = {
+    screens: "home"
+  };
+
+  onClickMoveHome = () => {
+    this.setState({ screens: "home" });
+  };
+  onClickMoveUser = () => {
+    this.setState({ screens: "souUsuario" });
+  };
+  onClickMoveProfessional = () => {
+    this.setState({ screens: "souProfissional" });
+  };
+
+  selectCurrentScreen = () => {
+    switch (this.state.screens) {
+      case "home":
+        return (
+          <Home
+            name={this.onClickMoveHome}
+            name1={this.onClickMoveUser}
+            name2={this.onClickMoveProfessional}
+          />
+        );
+      case "souUsuario":
+        return <Services />;
+      case "souProfissional":
+        return <NovaPagina />;
+      default:
+        return <Home />;
+    }
+  };
+
   render() {
-    return (
-      <div>
-        <p>Pronto para começar!</p>
-      </div>
-    )
+    return <div>{this.selectCurrentScreen()}</div>;
   }
 }
+
+export default App;
---
+++ src/components/NovaPagina.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class NovaPagina extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>essa é outra pagina</h1>
+      </div>
+    );
+  }
+}
